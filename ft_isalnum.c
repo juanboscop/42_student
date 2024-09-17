edc7f55d2371acd2c73e4cc329e2fe787b77f330
juanboscop <94535709+juanboscop@users.noreply.github.com>
2024-09-17 12:37:30 +0200

    add files from home

---
+++ ft_isalnum.c
@@ -0,0 +1,11 @@
+
+int ft_isalnum(int c)
+{
+    if((c <= 'z' && c >= 'a') || (c <= 'Z' && c >= 'A'))
+        return 1;
+    1
+    else if(c <= '9' && c >= '0')
+        return 1;
+    else
+        return 0;
+}
---
+++ ft_isalpha.c
@@ -0,0 +1,10 @@
+
+
+int ft_isalpha(int c)
+{
+    if((c <= 'z' && c >= 'a') || (c <= 'Z' && c >= 'A'))
+        return 1;
+
+    else
+        return 0;
+}
